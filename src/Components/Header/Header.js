ca884df4f22d93035211d6681d11c422516c1a73
Jackie Wong <wongzjackie@gmail.com>
2022-11-30 12:16:00 -1000

    Update styling and add animations to Header.
    -Update the styling for the Header component.
    -Make the Header component responsive to most screen sizes.
    -Add left/right padding to the Header component for improved spacing at different screen sizes.
    -Add "motion" to the Header.js file to animate the Header component's elements with framer motion.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import './index.css';
 
 const Header = () => {
   return (
-    <div className='header'>
-      <p>Hello, my name is</p>
-      <h1>Jackie Wong.</h1>
-      <h1>I build projects for the web.</h1>
-      <p className='caption'>
-        I am a self-taught web developer based in Honolulu, HI specializing in
-        building modern websites, web apps, and all else on the web.
-      </p>
-    </div>
+    <motion.div
+      className='header'
+      transition={{ staggerChildren: 0.05, delayChildren: 0.2 }}
+    >
+      <motion.div
+        initial={{ opacity: 0, y: 22 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.9, ease: 'easeInOut', type: 'spring' }}
+      >
+        <p>Hello, my name is</p>
+        <h1>Jackie Wong.</h1>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 15 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.7,
+          ease: 'easeInOut',
+          type: 'spring',
+          delay: 0.4,
+        }}
+      >
+        <h1>I build projects for the web.</h1>
+        <p className='caption'>
+          I am a self-taught web developer based in Honolulu, HI specializing in
+          building modern websites, web apps, and all else on the web.
+        </p>
+      </motion.div>
+    </motion.div>
   );
 };
 
