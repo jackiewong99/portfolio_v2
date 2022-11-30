23b3890c48f4afe76a44b1109a66430a8963cf9d
Jackie Wong <wongzjackie@gmail.com>
2022-11-29 21:46:44 -1000

    Create Header component.
    -Create Header component (first-top section of portfolio)

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <p>Hello, my name is</p>
+      <h1>Jackie Wong.</h1>
+      <h1>I build projects for the web.</h1>
+      <p className='caption'>
+        I am a self-taught web developer based in Honolulu, HI specializing in
+        building modern websites, web apps, and all else on the web.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
