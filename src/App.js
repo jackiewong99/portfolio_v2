7fcce74a45805dc51a0c62b1a8d53d50e7a02647
Jackie Wong <wongzjackie@gmail.com>
2023-01-11 00:36:04 -1000

    Update App code organization.
    - Clean up the organization of imports in the App component.
    - Import and add the About component to the App component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+// Components
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
 // Styling
 import './App.css';
-
-import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <About />
     </div>
   );
 }
