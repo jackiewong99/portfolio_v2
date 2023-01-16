3dcf7d032315b43593d00d40fa4f20d10dc9ad38
Jackie Wong <wongzjackie@gmail.com>
2023-01-16 00:56:41 -1000

    Create Projects component.
    - Create the header of the Projects component.
    - Style the container of the Projects component section and style the header text.

---
+++ src/Components/Projects/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Styling
+import styles from './Projects.module.css';
+
+const Projects = () => {
+  return (
+    <div className={styles.projects}>
+      <header>
+        <h1 className={styles.projectsHeader}>Projects</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Projects;
