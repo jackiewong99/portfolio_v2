8fac77922f2dc61d24f24690cc0929b3fcf8d156
Jackie Wong <wongzjackie@gmail.com>
2023-01-18 00:45:52 -1000

    Create ProjectCard component.
    - Create initial code for ProjectCard component.
    - Create the header of ProjectCard.
    - Apply initial styling of the header of the ProjectCard.

---
+++ src/Components/Projects/ProjectCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// Styling
+import styles from './Projects.module.css';
+
+const ProjectCard = () => {
+  return (
+    <div>
+      <header>
+        <h1 className={styles.cardHeader}>Projects</h1>
+      </header>
+    </div>
+  );
+};
+
+export default ProjectCard;
