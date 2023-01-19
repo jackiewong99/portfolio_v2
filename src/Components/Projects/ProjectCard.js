5887050e533df5a8399fee950576f9211ab26cec
Jackie Wong <wongzjackie@gmail.com>
2023-01-18 18:37:11 -1000

    Update ProjectCard.js
    - Add all elements/sections required for the ProjectCard component (header, description, tech list).
    - Add placeholder text for styling purposes.

--- src/Components/Projects/ProjectCard.js
+++ src/Components/Projects/ProjectCard.js
@@ -4,10 +4,25 @@ import styles from './Projects.module.css';
 
 const ProjectCard = () => {
   return (
-    <div>
+    <div className={styles.cardContainer}>
       <header>
-        <h1 className={styles.cardHeader}>Projects</h1>
+        <h2 className={styles.cardHeader}>Projects</h2>
       </header>
+      <section>
+        <div className={styles.cardDescription}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In id
+          dolores, porro aspernatur aut vitae expedita ab magni saepe doloremque
+          voluptas alias tempore iusto corporis exercitationem pariatur est
+          quisquam assumenda?
+        </div>
+      </section>
+      <section>
+        <ul className={styles.cardTechList}>
+          <li>Node.js</li>
+          <li>Node.js</li>
+          <li>Node.js</li>
+        </ul>
+      </section>
     </div>
   );
 };
