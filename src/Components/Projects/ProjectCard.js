13044fb34724a5d22a3f690e34def745c916b64b
Jackie Wong <wongzjackie@gmail.com>
2023-01-19 18:39:58 -1000

    Update ProjectCard
    - Add folder and GitHub icon from "react-feather" for the project cards.
    - Add props to be read into the project cards' content area.
    - Restructure elements for improved spacing.

--- src/Components/Projects/ProjectCard.js
+++ src/Components/Projects/ProjectCard.js
@@ -1,28 +1,51 @@
 import React from 'react';
+// Icons
+import { Folder, GitHub } from 'react-feather';
 // Styling
 import styles from './Projects.module.css';
 
-const ProjectCard = () => {
+const ProjectCard = ({
+  title,
+  description,
+  website,
+  github_repo,
+  tech_list,
+}) => {
   return (
     <div className={styles.cardContainer}>
       <header>
-        <h2 className={styles.cardHeader}>Projects</h2>
-      </header>
-      <section>
-        <div className={styles.cardDescription}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. In id
-          dolores, porro aspernatur aut vitae expedita ab magni saepe doloremque
-          voluptas alias tempore iusto corporis exercitationem pariatur est
-          quisquam assumenda?
+        <div className={styles.cardIcons}>
+          <div>
+            <a
+              href={website != '' ? website : github_repo}
+              target='_blank'
+              rel='noreferrer'
+              className={styles.cardLink}
+            >
+              <Folder color={`#5c8d89`} size={32} />
+            </a>
+          </div>
+          <div>
+            <a
+              href={github_repo}
+              target='_blank'
+              rel='noreferrer'
+              className={styles.cardLink}
+            >
+              <GitHub color={`#12714d99`} />
+            </a>
+          </div>
         </div>
-      </section>
-      <section>
+        <h2 className={styles.cardTitle}>{title}</h2>
+        <div className={styles.cardDescription}>{description}</div>
+      </header>
+      <footer>
         <ul className={styles.cardTechList}>
-          <li>Node.js</li>
-          <li>Node.js</li>
-          <li>Node.js</li>
+          {tech_list.map((tech, i) => (
+            <li key={i}>{tech}</li>
+          ))}
         </ul>
-      </section>
+      </footer>
     </div>
   );
 };
