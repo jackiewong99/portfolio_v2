310da58ccbd998ef257ae489a6b90f948946942b
Jackie Wong <wongzjackie@gmail.com>
2023-01-19 18:41:53 -1000

    Update main Projects component.
    - Add projectList from the "project_list.js" file.
    - Remove placeholder ProjectCard components.
    - Loop over list of projects and render each project as a ProjectCard component.

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ProjectCard from './ProjectCard';
 // Styling
 import styles from './Projects.module.css';
+// Project list
+import { projectList } from './project_list';
 
 const Projects = () => {
   return (
@@ -11,15 +13,18 @@ const Projects = () => {
       </header>
       <section>
         <ul className={styles.projectsGrid}>
-          <li>
-            <ProjectCard />
-          </li>
-          <li>
-            <ProjectCard />
-          </li>
-          <li>
-            <ProjectCard />
-          </li>
+          {projectList.map((project, i) => (
+            <li key={i}>
+              <ProjectCard
+                title={project.title}
+                description={project.description}
+                website={project.website}
+                github_repo={project.github_repo}
+                tech_list={project.tech_list}
+                key={i}
+              />
+            </li>
+          ))}
         </ul>
       </section>
     </div>
