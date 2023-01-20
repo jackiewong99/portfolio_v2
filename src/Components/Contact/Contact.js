9e1dd8732f4e2c5aa1a0516a1a8720dc5e4046b4
Jackie Wong <wongzjackie@gmail.com>
2023-01-20 00:09:10 -1000

    Create Contact.js

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <header>
+        <h1>Interested?</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Contact;
