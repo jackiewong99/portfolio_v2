8e2ae7a9b84232c49a3e2fcf405011a05f89517f
Jackie Wong <wongzjackie@gmail.com>
2023-01-20 15:21:19 -1000

    Create structure and add text to Contact.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,4 +1,3 @@
-import React from 'react';
 // Styling
 import styles from './Contact.module.css';
 
@@ -6,8 +5,18 @@ const Contact = () => {
   return (
     <div className={styles.contact}>
       <header>
-        <h1 className={styles.contactHeader}>Interested?</h1>
+        <h1 className={styles.contactHeader}>Say Hi</h1>
       </header>
+      <footer>
+        <div className={styles.contactFooter}>
+          <p className={styles.contactText}>
+            I'm always looking for new opportunities and my inbox is always open
+            to any messages. I will get back to any inquiries, questions, or any
+            other messages as soon as possible!
+          </p>
+          <button className={styles.contactBtn}>Get in Touch</button>
+        </div>
+      </footer>
     </div>
   );
 };
