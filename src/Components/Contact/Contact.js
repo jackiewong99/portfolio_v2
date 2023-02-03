2aa5dcea7e25a752e28cd3c9507a2d40983d2ae9
Jackie Wong <wongzjackie@gmail.com>
2023-02-02 17:06:23 -1000

    Update JSX structure for modal.

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -10,28 +10,26 @@ const Contact = () => {
   const { modalOpen, open, close } = useModal();
 
   return (
-    <AnimateSharedLayout type='crossfade'>
-      <div className={styles.contact}>
-        <header>
-          <h1 className={styles.contactHeader}>Say Hi</h1>
-        </header>
-        <footer>
-          <div className={styles.contactFooter}>
-            <p className={styles.contactText}>
-              I'm always looking for new opportunities and my inbox is always
-              open to any messages. I will get back to inquiries, questions, or
-              any other messages as soon as possible!
-            </p>
-            <motion.button className={styles.contactBtn} onClick={open}>
-              Get in Touch
-            </motion.button>
-          </div>
-        </footer>
-      </div>
+    <div className={styles.contact}>
+      <header>
+        <h1 className={styles.contactHeader}>Say Hi</h1>
+      </header>
+      <footer>
+        <div className={styles.contactFooter}>
+          <p className={styles.contactText}>
+            I'm always looking for new opportunities and my inbox is always open
+            to any messages. I will get back to inquiries, questions, or any
+            other messages as soon as possible!
+          </p>
+          <motion.button className={styles.contactBtn} onClick={open}>
+            Get in Touch
+          </motion.button>
+        </div>
+      </footer>
       <AnimatePresence initial={false}>
         {modalOpen && <Modal modalOpen={modalOpen} handleClose={close} />}
       </AnimatePresence>
-    </AnimateSharedLayout>
+    </div>
   );
 };
 
