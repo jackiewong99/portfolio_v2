ebb3a92686e4615ebbeecbec209b1d4ed1d81ef5
Jackie Wong <wongzjackie@gmail.com>
2023-02-08 01:08:43 -1000

    Update App.js
    - Add Nav component to App component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // Components
+import Nav from './Components/Nav/Nav';
 import Header from './Components/Header/Header';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
@@ -9,6 +10,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.App}>
+      <Nav />
       <Header />
       <About />
       <Projects />
