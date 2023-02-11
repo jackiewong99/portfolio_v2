6b18876ae32b5fa7dcc7620ee39eb8a11faf04c5
Jackie Wong <wongzjackie@gmail.com>
2023-02-10 19:38:38 -1000

    Update navbar
    - Reorganize import statements
    - Use "useScrollPosition" hook to trigger background color visibility of the navbar
    - Update styling of navbar background

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -1,11 +1,21 @@
 import { Link } from 'react-scroll';
 import { navLabels } from './nav_labels';
-import resume from '../../assets/Jackie_Wong_Resume.pdf';
+// Hook
+import useScrollPosition from '../../hooks/useScrollPosition';
+// Styling
 import styles from './Nav.module.css';
+// Asset
+import resume from '../../assets/Jackie_Wong_Resume.pdf';
 
 const Navbar = () => {
+  const scrollPosition = useScrollPosition();
+
   return (
-    <nav className={styles.navbarContainer}>
+    <nav
+      className={`${styles.navbarContainer} ${
+        scrollPosition > 0 ? styles.navbarBg : ''
+      }`}
+    >
       <header className={styles.navHeader}>
         <a href='/' className={styles.navHeaderLink}>
           JACKIE WONG
