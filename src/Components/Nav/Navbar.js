90bcce6616132de695bcaabf15e0f713831f3366
Jackie Wong <wongzjackie@gmail.com>
2023-02-13 21:59:23 -1000

    Update Navbar.js
    - Import useEffect, useState, motion, and debounce helper function
    - Create animation variants for the parent navbar element and child nav items
    - Integrate useEffect hook for tracking the window's scroll position
    - Create state for applying background color styling to the navbar, and hiding/showing the navbar
    - Animate all of the navbar items
    - Stagger the navbar items' animations (excluding the header)

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
+import { motion } from 'framer-motion';
 import { navLabels } from './nav_labels';
+import debounce from '../../util/helpers';
 // Hook
 import useScrollPosition from '../../hooks/useScrollPosition';
 // Styling
@@ -7,24 +10,75 @@ import styles from './Nav.module.css';
 // Asset
 import resume from '../../assets/Jackie_Wong_Resume.pdf';
 
+const navbarParentVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: { staggerChildren: 0.2, delayChildren: 0.2 },
+  },
+};
+
+const navbarItemVariants = {
+  hidden: {
+    y: -100,
+  },
+  visible: {
+    y: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 500,
+      damping: 100,
+      duration: 0.7,
+    },
+  },
+};
+
 const Navbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isVisible, setIsVisible] = useState(true);
   const scrollPosition = useScrollPosition();
 
+  useEffect(() => {
+    const handlePrevScrollPos = debounce(() => {
+      const currentPos = window.scrollY;
+
+      setIsVisible(
+        (prevScrollPos > currentPos && prevScrollPos - currentPos > 70) ||
+          currentPos < 10,
+      );
+      setPrevScrollPos(currentPos);
+    }, 100);
+
+    window.addEventListener('scroll', handlePrevScrollPos);
+    return () => window.removeEventListener('scroll', handlePrevScrollPos);
+  }, [prevScrollPos]);
+
   return (
-    <nav
+    <motion.nav
       className={`${styles.navbarContainer} ${
         scrollPosition > 0 ? styles.navbarBg : ''
-      }`}
+      } ${isVisible ? styles.navbarVisible : styles.navbarHide}`}
     >
-      <header className={styles.navHeader}>
+      <motion.header
+        className={styles.navHeader}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1, transition: { duration: 0.3 } }}
+      >
         <a href='/' className={styles.navHeaderLink}>
           JACKIE WONG
         </a>
-      </header>
-      <ul className={styles.navLinks}>
+      </motion.header>
+      <motion.ul
+        className={styles.navLinks}
+        variants={navbarParentVariants}
+        initial='hidden'
+        animate='visible'
+      >
         {navLabels.map((label, key) =>
           label.name !== 'RESUME' ? (
-            <li key={key}>
+            <motion.li key={key} variants={navbarItemVariants}>
               <Link
                 activeClass={styles.active}
                 to={label.url}
@@ -35,9 +89,9 @@ const Navbar = () => {
               >
                 {label.name}
               </Link>
-            </li>
+            </motion.li>
           ) : (
-            <li key={key}>
+            <motion.li key={key} variants={navbarItemVariants}>
               <a
                 href={resume}
                 target='_blank'
@@ -46,11 +100,11 @@ const Navbar = () => {
               >
                 RESUME
               </a>
-            </li>
+            </motion.li>
           ),
         )}
-      </ul>
-    </nav>
+      </motion.ul>
+    </motion.nav>
   );
 };
 
