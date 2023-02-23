5c3357e4e952967563c8241e3cc79d20b0e07a70
Jackie Wong <wongzjackie@gmail.com>
2023-02-23 00:02:57 -1000

    Update Navbar.js
    - Add responsive feature to nav.
    - Use "useScreenWidth" hook to conditionally render the navbar and mobile nav menu based on the current screen width.
    - Use 'useScreenWidth" hook to style different navbar background color based on specific nav menu that is currently rendered.
    - Add opacity animation to the header of the navbar.

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react';
+// Packages
 import { Link } from 'react-scroll';
 import { motion } from 'framer-motion';
+// Component
+import MobileNav from './MobileNav';
+// Hooks and utilities
+import { useEffect, useState } from 'react';
+import useScrollPosition from '../../hooks/useScrollPosition';
+import useScreenWidth from '../../hooks/useScreenWidth';
 import { navLabels } from './nav_labels';
 import debounce from '../../util/helpers';
-// Hook
-import useScrollPosition from '../../hooks/useScrollPosition';
 // Styling
 import styles from './Nav.module.css';
 // Asset
@@ -39,6 +43,7 @@ const Navbar = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [isVisible, setIsVisible] = useState(true);
   const scrollPosition = useScrollPosition();
+  const screenWidthMatch = useScreenWidth();
 
   useEffect(() => {
     const handlePrevScrollPos = debounce(() => {
@@ -58,52 +63,58 @@ const Navbar = () => {
   return (
     <motion.nav
       className={`${styles.navbarContainer} ${
-        scrollPosition > 0 ? styles.navbarBg : ''
-      } ${isVisible ? styles.navbarVisible : styles.navbarHide}`}
+        scrollPosition && screenWidthMatch > 0 ? styles.navbarBg : ''
+      } ${!screenWidthMatch ? styles.mobileNavbarBg : ''} ${
+        isVisible ? styles.navbarVisible : styles.navbarHide
+      }`}
     >
       <motion.header
         className={styles.navHeader}
         initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 0.3 } }}
+        animate={{ opacity: 1, transition: { duration: 0.7 } }}
       >
         <a href='/' className={styles.navHeaderLink}>
           JACKIE WONG
         </a>
       </motion.header>
-      <motion.ul
-        className={styles.navLinks}
-        variants={navbarParentVariants}
-        initial='hidden'
-        animate='visible'
-      >
-        {navLabels.map((label, key) =>
-          label.name !== 'RESUME' ? (
-            <motion.li key={key} variants={navbarItemVariants}>
-              <Link
-                activeClass={styles.active}
-                to={label.url}
-                spy={true}
-                smooth={true}
-                duration={500}
-                className={styles.navLink}
-              >
-                {label.name}
-              </Link>
-            </motion.li>
-          ) : (
-            <motion.li key={key} variants={navbarItemVariants}>
-              <a
-                href={resume}
-                target='_blank'
-                rel='noreferrer'
-                className={styles.navLink}
-              >
-                RESUME
-              </a>
-            </motion.li>
-          ),
-        )}
-      </motion.ul>
+      {screenWidthMatch ? (
+        <motion.ul
+          className={styles.navLinks}
+          variants={navbarParentVariants}
+          initial='hidden'
+          animate='visible'
+        >
+          {navLabels.map((label, key) =>
+            label.name !== 'RESUME' ? (
+              <motion.li key={key} variants={navbarItemVariants}>
+                <Link
+                  activeClass={styles.active}
+                  to={label.url}
+                  spy={true}
+                  smooth={true}
+                  duration={500}
+                  className={styles.navLink}
+                >
+                  {label.name}
+                </Link>
+              </motion.li>
+            ) : (
+              <motion.li key={key} variants={navbarItemVariants}>
+                <a
+                  href={resume}
+                  target='_blank'
+                  rel='noreferrer'
+                  className={styles.navLink}
+                >
+                  RESUME
+                </a>
+              </motion.li>
+            ),
+          )}
+        </motion.ul>
+      ) : (
+        <MobileNav />
+      )}
     </motion.nav>
   );
 };
