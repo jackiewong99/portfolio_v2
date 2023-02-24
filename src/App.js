9dc46f55c878752499b6b65bd6e137c2af607901
Jackie Wong <wongzjackie@gmail.com>
2023-02-24 00:40:12 -1000

    Update App.js
    - Render the Footer component in App.js file.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header/Header';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
 import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 // Styling
 import styles from './App.module.css';
 
@@ -15,6 +16,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   );
 }
