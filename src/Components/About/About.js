8bfe4800a2cb36a6d199f4d447df96c253397568
Jackie Wong <wongzjackie@gmail.com>
2023-02-24 20:45:56 -1000

    Add animation to About.js
    - Add animation to the About component when it is initially in the viewport.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,19 +5,39 @@ import AboutImage from './AboutImage';
 import styles from './About.module.css';
 import { motion } from 'framer-motion';
 
+const aboutVariants = {
+  offscreen: {
+    opacity: 0,
+    y: 40,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
 const About = () => {
   return (
-    <div name='about' className={styles.about}>
-      <header>
+    <motion.div
+      name='about'
+      className={styles.about}
+      initial='offscreen'
+      whileInView='onscreen'
+      viewport={{ once: true, amount: 0.5 }}
+    >
+      <motion.header variants={aboutVariants}>
         <h1 className={styles.aboutHeader}>About Me</h1>
-      </header>
-      <main>
+      </motion.header>
+      <motion.main variants={aboutVariants}>
         <div className={styles.container}>
           <AboutImage />
           <AboutDetail />
         </div>
-      </main>
-    </div>
+      </motion.main>
+    </motion.div>
   );
 };
 
