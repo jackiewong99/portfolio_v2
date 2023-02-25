d6f9db50330229052173fbef33671c4a37c9d0bb
Jackie Wong <wongzjackie@gmail.com>
2023-02-24 21:04:04 -1000

    Refactor scroll animation variants.
    - Refactor reusable scroll animation variants for the About and Contact components.

---
+++ src/util/animation.js
@@ -0,0 +1,13 @@
+export const scrollAnimVariants = {
+  offscreen: {
+    opacity: 0,
+    y: 40,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
